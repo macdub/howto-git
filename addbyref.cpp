b57b0bff0827b6e6454918986c74f76a6acefc55
mcdojoh <john.mcdowell@mcdojoh.com>
2023-04-19 15:55:33 -0500

    init test file

---
+++ addbyref.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+void addOneByRef(int& val) {
+    val++;
+}
+
+int main() {
+    int x = 3;
+
+    printf("X is %d\n", x);
+
+    addOneByRef(x);
+
+    printf("X is %d\n", x);
+}
